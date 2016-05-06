66aa5bbe503a02ff40201c7769a6e1bc89ea9849
Radu Sepetan <rasepeta@uwaterloo.ca>
2016-05-06 15:05:47 -0400

    added login validation

---
+++ assets/js/login.js
@@ -0,0 +1,30 @@
+function hideMessage() {
+	$('#error-message').fadeOut();
+}
+
+function validateForm() {
+    var url = document.forms["login"]["profileUrl"].value;
+    if(url.substring(url.length-1, url.length) == '/'){
+    	url = url.substring(0, url.length-1)
+    }
+    // console.log(url);
+    
+	var parts = url.split("/");
+	if(parts.length > 3){
+	    var name = parts[parts.length - 1];
+	    var realm = parts[parts.length - 2];
+	    var id = parts[parts.length - 3];
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "https://us.api.battle.net/sc2/profile/"+id+"/"+realm+"/"+name+"/?locale=en_US&apikey=tb68txxwdq4vwxtwah7gkww6uqebxn9v", false);
+	   	xhr.send();
+	   	if(xhr.status != 200){
+	   		$('#error-message').show();
+	   		setTimeout(hideMessage, 3000);
+	   		return false;
+	   	}
+	}
+	else{
+		return false;
+	}
+}
+
--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -14,7 +14,7 @@
 
   <body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- <script src="../../../../assets/js/login.js"></script> -->
+    <script src="../../../../assets/js/login.js"></script>
     <div class="main-background">
       <div class="login-card">
         <div class="login-container">
@@ -23,10 +23,13 @@
             <img src="/images/title.png" alt="SC2tracker">
           </div>
 
-          <form action="" method="post">
+          <form name="login" action="" method="post" onsubmit="return validateForm()">
             <div class="form-group">
               <label for="login-url">SC2 PROFILE URL</label>
               <input type="text" class="form-control" id="InputUrl" name="profileUrl" placeholder="URL">
+            </div>
+            <div id="error-message" style="display:none;">
+              The URL you provided is not valid
             </div>
             <!-- <div class="form-group">
               <label for="login-email">EMAIL</label>
@@ -37,7 +40,7 @@
               <input type="password" class="form-control" id="InputPassword" placeholder="PASSWORD">
             </div> -->
             <div class="form-group">
-              <button type="submit" class="btn btn-default" name="submit" onclick="<?php echo Yii::app()->createUrl('site/index')?>">Submit</button>
+              <button type="submit" class="btn btn-default" name="submit">Submit</button>
             </div>
           </form>
 
