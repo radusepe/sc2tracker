6b28cfc42a67348736e9270a8eb4a4993e5e58ae
Radu Sepetan <rasepeta@uwaterloo.ca>
2016-05-10 17:39:47 -0400

    app icon added

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
 	<link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon-32x32.png" sizes="32x32" />
 	<link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon-16x16.png" sizes="16x16" />
+	<link rel="apple-touch-icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/app.png"/>
 	<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
 	<![endif]-->
